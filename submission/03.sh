018d54161f6993692fb1857402b68b41f2db8749
StephenChi-hi <nwikechisom77@gmail.com>
2026-03-25 06:04:09 +0100

    update scripts

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,10 @@
 # Create a SegWit address.
+address=$(bitcoin-cli getnewaddress "" bech32)
+
+
 # Add funds to the address.
+bitcoin-cli generatetoaddress 101 $address > /dev/null
+
 # Return only the Address
+echo $address
+
--- submission/08.sh
+++ submission/08.sh
@@ -2,3 +2,22 @@
 # The transaction fee should be 20,000 satoshis, then the rest of the amount should be sent to the address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP
 # Use the UTXOs from the transaction below
 # raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+
+raw_tx="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+recipient_address="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+
+# get txid
+txid=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -r .txid)
+
+# sum of all outputs
+total_value=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq '[.vout[].value] | map(. * 100000000 | round) | add')
+
+# output value total - 20,000 satoshi fee
+fee=20000
+output_value=$(echo "scale=8; ($total_value - $fee) / 100000000" | bc)
+
+# inputs 
+inputs=$(bitcoin-cli -regtest decoderawtransaction "$raw_tx" | jq -c '[.vout | to_entries[] | {"txid":"'$txid'","vout":.key}]')
+
+# raw transaction
+bitcoin-cli -regtest createrawtransaction "$inputs" '{"'$recipient_address'":'$output_value'}'
